5b53497e9670d982be34df27fa4a172406d0d189
Valentin <valtih1978@gmail.com>
2012-01-04 15:27:40 +0200

    Can touch any file to build.
    It is a bit simpler and more universal. At least aids in build logic analysis.

--- src/org/zamia/plugin/ui/ResourceToucher.java
+++ src/org/zamia/plugin/ui/ResourceToucher.java
@@ -10,14 +10,14 @@ import org.eclipse.jface.viewers.ITreeSelection;
 import org.eclipse.jface.viewers.TreeSelection;
 import org.eclipse.ui.handlers.HandlerUtil;
 
-@SuppressWarnings("deprecation")
-public class SetBuildPathHandler extends AbstractHandler{
+/**Touching resources must start building them. If builpath file is changed (touched), it becomes active. */
+public class ResourceToucher extends AbstractHandler{
 
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		ITreeSelection v = (ITreeSelection) HandlerUtil.getVariable(event, "selection");
 		try {
-			((IFile) v.getFirstElement()).touch(null);
+			((IResource) v.getFirstElement()).touch(null);
 		} catch (CoreException e1) {
 			throw new ExecutionException("Failed to touch " + v, e1);
 		}
