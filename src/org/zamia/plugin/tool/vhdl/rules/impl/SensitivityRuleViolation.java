a57d3ce4b6eba47e1f32321fd217a69ea16ec1dd
mblondeau2 <matthieu.blondeau@altran.com>
2017-11-07 17:20:02 +0100

    Fix missing file for SonarError tags

--- src/org/zamia/plugin/tool/vhdl/rules/impl/SensitivityRuleViolation.java
+++ src/org/zamia/plugin/tool/vhdl/rules/impl/SensitivityRuleViolation.java
@@ -13,14 +13,16 @@ public class SensitivityRuleViolation {
 	private Architecture _architecture; 
 	private Process _process;
 	private String _sensitivityName;
+	private boolean _sensitivityMissing;
 	
-	public SensitivityRuleViolation(String fileName, int line, Entity entity, Architecture architecture, Process process, String sensitivityName) {
+	public SensitivityRuleViolation(String fileName, int line, Entity entity, Architecture architecture, Process process, String sensitivityName, boolean sensitivityMissing) {
 		_fileName = fileName;
 		_line = line;
 		_entity = entity;
 		_architecture = architecture;
 		_process = process;
 		_sensitivityName = sensitivityName;
+		_sensitivityMissing = sensitivityMissing;
 	}
 	
 	public void generate(ReportFile reportFile) {
@@ -31,5 +33,16 @@ public class SensitivityRuleViolation {
 		String processId = _process.getLabel(); 
 		reportFile.addElement(ReportFile.TAG_PROCESS, processId, info); 
 		reportFile.addElement(ReportFile.TAG_SENSITIVITY, _sensitivityName, info); 
+		
+		if (_sensitivityMissing)
+		{
+			reportFile.addElement(ReportFile.TAG_SONAR_ERROR, "Signal " + _sensitivityName + " is not in the sensitivity list of the synchronous process", info);
+			reportFile.addElement(ReportFile.TAG_SONAR_MSG, "Add " + _sensitivityName + " in the sensitivity list of " + processId, info);
+		}
+		else
+		{
+			reportFile.addElement(ReportFile.TAG_SONAR_ERROR, "Signal " + _sensitivityName + " should not be in the sensitivity list of the synchronous process", info);
+			reportFile.addElement(ReportFile.TAG_SONAR_MSG, "Remove " + _sensitivityName + " from the sensitivity list of " + processId, info);
+		}
 	}
 }
