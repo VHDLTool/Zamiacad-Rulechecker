d2cbe632043c2e511275fdb9f571b4be81b3bc43
EUROPE\xsheng <xinheng.sheng@altran.com>
2019-01-14 16:24:27 +0100

    WIP: add a base part

--- src/org/zamia/plugin/tool/vhdl/rules/impl/std/RuleSTD_01100.java
+++ src/org/zamia/plugin/tool/vhdl/rules/impl/std/RuleSTD_01100.java
@@ -8,12 +8,13 @@ import org.zamia.util.Pair;
 
 public class RuleSTD_01100 extends Rule {
 
-	protected RuleSTD_01100() {
+	public RuleSTD_01100() {
 		super(RuleE.STD_01100);
 	}
 
 	@Override
 	public Pair<Integer, RuleResult> Launch(ZamiaProject zPrj, String ruleId, ParameterSource parameterSource) {
+		initializeRule(parameterSource, ruleId);
 		return null;
 	}
 
