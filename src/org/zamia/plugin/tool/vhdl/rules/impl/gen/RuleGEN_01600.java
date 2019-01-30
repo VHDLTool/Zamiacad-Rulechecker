196fdd731f3c7ca2e559508485711e99217b7b01
EUROPE\xsheng <xinheng.sheng@altran.com>
2019-01-30 15:40:50 +0100

    WIP

--- src/org/zamia/plugin/tool/vhdl/rules/impl/gen/RuleGEN_01600.java
+++ src/org/zamia/plugin/tool/vhdl/rules/impl/gen/RuleGEN_01600.java
@@ -1,11 +1,18 @@
 package org.zamia.plugin.tool.vhdl.rules.impl.gen;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 import org.zamia.ZamiaProject;
 import org.zamia.plugin.tool.vhdl.rules.IHandbookParam;
 import org.zamia.plugin.tool.vhdl.rules.RuleE;
 import org.zamia.plugin.tool.vhdl.rules.RuleResult;
+import org.zamia.plugin.tool.vhdl.rules.StringParam;
 import org.zamia.plugin.tool.vhdl.rules.impl.Rule;
 import org.zamia.util.Pair;
 
@@ -21,13 +28,29 @@ public class RuleGEN_01600 extends Rule{
 	public Pair<Integer, RuleResult> Launch(ZamiaProject zPrj, String ruleId, ParameterSource parameterSource) {
 		initializeRule(parameterSource, ruleId);
 		
-		List<IHandbookParam> parameterList = getParameterList(zPrj);
-		if (parameterList == null && withParameter) {
-			return new Pair<Integer, RuleResult> (WRONG_PARAM, null);
-		} else if (parameterList != null && withParameter) {
-			
+		List<IHandbookParam> parameterList;
+		if (withParameter) {
+			parameterList = getParameterList(zPrj);
+			if (parameterList == null) {
+				return new Pair<> (WRONG_PARAM, null);
+			}
+		} else {
+			parameterList = getDefaultList();
 		}
 		
+		return null;
+	}
+	
+	private List<IHandbookParam> getDefaultList(){
+		try {
+			Document document = DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument();
+			Element root = document.createElement("hb:RuleParams");
+			Element params = document.createElement("hb:StringParam");
+//			params.appendChild(document.create)
+			document.appendChild(root);
+		} catch (ParserConfigurationException e) {
+			e.printStackTrace();
+		}
 		return null;
 	}
 
