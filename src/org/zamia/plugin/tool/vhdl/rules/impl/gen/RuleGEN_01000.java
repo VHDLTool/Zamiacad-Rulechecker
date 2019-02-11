6e1bd739488e081ec8f8a18dc6e3233f6f28414d
EUROPE\xsheng <xinheng.sheng@altran.com>
2019-02-11 15:01:13 +0100

    Extract subprogrammanager comment lines to disable parameters

--- src/org/zamia/plugin/tool/vhdl/rules/impl/gen/RuleGEN_01000.java
+++ src/org/zamia/plugin/tool/vhdl/rules/impl/gen/RuleGEN_01000.java
@@ -13,33 +13,26 @@ import org.zamia.ZamiaProject;
 import org.zamia.plugin.tool.vhdl.EntityException;
 import org.zamia.plugin.tool.vhdl.HdlArchitecture;
 import org.zamia.plugin.tool.vhdl.HdlEntity;
-import org.zamia.plugin.tool.vhdl.HdlFile;
+import org.zamia.plugin.tool.vhdl.HdlSubProgram;
+import org.zamia.plugin.tool.vhdl.HdlSubProgram.TYPE_SUBPROGRAM;
 import org.zamia.plugin.tool.vhdl.Process;
 import org.zamia.plugin.tool.vhdl.ReportFile;
-import org.zamia.plugin.tool.vhdl.manager.ArchitectureManager;
-import org.zamia.plugin.tool.vhdl.manager.PackageBodyManager;
+import org.zamia.plugin.tool.vhdl.manager.SubProgramManager;
 import org.zamia.plugin.tool.vhdl.rules.IHandbookParam;
 import org.zamia.plugin.tool.vhdl.rules.RuleE;
 import org.zamia.plugin.tool.vhdl.rules.RuleResult;
 import org.zamia.plugin.tool.vhdl.rules.impl.Rule;
 import org.zamia.plugin.tool.vhdl.rules.impl.SonarQubeRule;
 import org.zamia.util.Pair;
-import org.zamia.vhdl.ast.Architecture;
 import org.zamia.vhdl.ast.BlockDeclarativeItem;
-import org.zamia.vhdl.ast.Entity;
-import org.zamia.vhdl.ast.PackageBody;
 import org.zamia.vhdl.ast.SequentialProcess;
 import org.zamia.vhdl.ast.SubProgram;
-import org.zamia.vhdl.ast.VHDLPackage;
 import org.zamia.vhdl.ast.VariableDeclaration;
 
 public class RuleGEN_01000 extends Rule {
 	
 	private static final String POSITION = "Prefix";
 	private static final String VALUE = "v_";
-	
-	private ReportFile reportFile = new ReportFile(this);
-	private List<IHandbookParam> parameterList = null;
 
 	public RuleGEN_01000() {
 		super(RuleE.GEN_01000);
@@ -48,8 +41,12 @@ public class RuleGEN_01000 extends Rule {
 	@Override
 	public Pair<Integer, RuleResult> Launch(ZamiaProject zPrj, String ruleId, ParameterSource parameterSource) {
 		initializeRule(parameterSource, ruleId);
+	
+		ReportFile reportFile = new ReportFile(this);
+		List<IHandbookParam> parameterList = null;
 		
-		parameterList = getParameterList(zPrj);
+		// Uncomment this line to enable parameters
+//		parameterList = getParameterList(zPrj);
 		if (parameterList == null || parameterList.isEmpty()) {
 			parameterList = getDefaultStringParamList(POSITION, VALUE);
 		}
@@ -57,46 +54,45 @@ public class RuleGEN_01000 extends Rule {
 		Pair<Integer, RuleResult> result = null;
 
 		if (reportFile.initialize()) {
-			// search variable in function
 			try {
-				PackageBodyManager.getPackageBody();
-				PackageBodyManager.resetInfo();
-				Map<String, HdlFile> hdlFiles = ArchitectureManager.getArchitecture();
-				for (Entry<String, HdlFile> entry: hdlFiles.entrySet()) {
-					HdlFile hdlFile = entry.getValue();
-					for (HdlEntity hdlEntity: hdlFile.getListHdlEntity()) {
-						// search function in entity
-						List<BlockDeclarativeItem> items = hdlEntity.getEntity().fDeclarations;
-						for (BlockDeclarativeItem item: items) {
-							if (item instanceof SubProgram) {
-								searchVariableInSubProgram((SubProgram)item, hdlEntity.getEntity(), null);
+				List<HdlSubProgram> hdlSubPrograms = SubProgramManager.getSubProgram();
+				for (HdlSubProgram hdlSubProgram : hdlSubPrograms) {
+					SubProgram subProgram = hdlSubProgram.getSubProgram();
+					HdlEntity hdlEntity = hdlSubProgram.getExtraInfo().getHdlEntity();
+					HdlArchitecture hdlArchitecture = hdlSubProgram.getExtraInfo().getHdlArchitecture();
+					for (int i = 0; i < subProgram.getNumDeclarations(); i++) {
+						BlockDeclarativeItem item = subProgram.getDeclaration(i);
+						if (item instanceof VariableDeclaration) {
+							boolean isValid = false;
+							for (IHandbookParam param: parameterList) {
+								isValid |= param.isValid(item.getId());
 							}
-						}
-						// search function in architecture
-						for (HdlArchitecture hdlArchitecture: hdlEntity.getListHdlArchitecture()) {
-							List<BlockDeclarativeItem> itemList = hdlArchitecture.getArchitecture().fDeclarations;
-							for (BlockDeclarativeItem item: itemList) {
-								if (item instanceof SubProgram) {
-									searchVariableInSubProgram((SubProgram)item, hdlEntity.getEntity(), hdlArchitecture.getArchitecture());
+							if (!isValid) {
+								Element element;
+								if (hdlEntity == null) {
+									element = reportFile.addViolation(item.getLocation());
+								} else if (hdlArchitecture == null) {
+									element = reportFile.addViolation(
+										item.getLocation(),
+										hdlEntity.getEntity().getId(),
+										""
+										);
+								} else {
+									element = reportFile.addViolation(
+											item.getLocation(),
+											hdlEntity.getEntity(),
+											hdlArchitecture.getArchitecture()
+											);
 								}
-							}
-						}
-					}
-					// search function in package
-					for (VHDLPackage vhdlPackage: hdlFile.getListHdlPackage()) {
-						List<BlockDeclarativeItem> items = vhdlPackage.fDeclarations;
-						for (BlockDeclarativeItem item: items) {
-							if (item instanceof SubProgram) {
-								searchVariableInSubProgram((SubProgram)item, null, null);
-							}
-						}
-					}
-					// search function in package body
-					for (PackageBody packageBody: hdlFile.getListPackageBody()) {
-						List<BlockDeclarativeItem> items = packageBody.fDeclarations;
-						for (BlockDeclarativeItem item: items) {
-							if (item instanceof SubProgram) {
-								searchVariableInSubProgram((SubProgram)item, null, null);
+								reportFile.addElement(ReportFile.TAG_PROCESS, null, element);
+								reportFile.addElement(ReportFile.TAG_FUNCTION, hdlSubProgram.getType() == TYPE_SUBPROGRAM.FUNCTION ? subProgram.getId() : null, element);
+								reportFile.addElement(ReportFile.TAG_PROCEDURE, hdlSubProgram.getType() == TYPE_SUBPROGRAM.PROCEDURE ? subProgram.getId() : null, element);
+								reportFile.addElement(ReportFile.TAG_VARIABLE, item.getId(), element);
+								reportFile.addSonarTags(element,
+										SonarQubeRule.SONAR_ERROR_GEN_01000,
+										new Object[] {item.getId()},
+										SonarQubeRule.SONAR_MSG_GEN_01000,
+										new Object[] {item.getId(), VALUE, POSITION.toLowerCase()});
 							}
 						}
 					}
@@ -143,10 +139,6 @@ public class RuleGEN_01000 extends Rule {
 										SonarQubeRule.SONAR_MSG_GEN_01000,
 										new Object[] {variable.getId(), VALUE, POSITION.toLowerCase()});
 							}
-						} 
-						// search function in process
-						else if (variable instanceof SubProgram) {
-							searchVariableInSubProgram((SubProgram) variable, entry.getValue().getEntity(), entry.getValue().getArchitecture());
 						}
 					}
 				}
@@ -154,48 +146,6 @@ public class RuleGEN_01000 extends Rule {
 			result = reportFile.save();
 		}
 		return result;
-	}
-	
-	private void searchVariableInSubProgram(SubProgram subProgram, Entity entity, Architecture architecture) {
-		for (int i = 0; i < subProgram.getNumDeclarations(); i++) {
-			BlockDeclarativeItem item = subProgram.getDeclaration(i);
-			if (item instanceof SubProgram) {
-				// search function in function
-				searchVariableInSubProgram((SubProgram) item, entity, architecture);
-			} else if (item instanceof VariableDeclaration) {
-				boolean isValid = false;
-				for (IHandbookParam param: parameterList) {
-					isValid |= param.isValid(item.getId());
-				}
-				if (!isValid) {
-					Element element;
-					if (entity == null) {
-						element = reportFile.addViolation(item.getLocation());
-					} else if (architecture == null) {
-						element = reportFile.addViolation(
-							item.getLocation(),
-							entity.getId(),
-							""
-							);
-					} else {
-						element = reportFile.addViolation(
-								item.getLocation(),
-								entity,
-								architecture
-								);
-					}
-					reportFile.addElement(ReportFile.TAG_PROCESS, null, element);
-					reportFile.addElement(ReportFile.TAG_FUNCTION, subProgram.getChild(0) == null ? null : subProgram.getId(), element);
-					reportFile.addElement(ReportFile.TAG_PROCEDURE, subProgram.getChild(0) == null ? subProgram.getId() : null, element);
-					reportFile.addElement(ReportFile.TAG_VARIABLE, item.getId(), element);
-					reportFile.addSonarTags(element,
-							SonarQubeRule.SONAR_ERROR_GEN_01000,
-							new Object[] {item.getId()},
-							SonarQubeRule.SONAR_MSG_GEN_01000,
-							new Object[] {item.getId(), VALUE, POSITION.toLowerCase()});
-				}
-			}
-		}
 	}
 
 }
