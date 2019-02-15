87134d100859e37ce56b26bca24e2ccd24c9e39d
EUROPE\xsheng <xinheng.sheng@altran.com>
2019-02-15 16:41:35 +0100

    add basic part

---
+++ src/org/zamia/plugin/tool/vhdl/rules/impl/std/RuleSTD_05900.java
@@ -0,0 +1,21 @@
+package org.zamia.plugin.tool.vhdl.rules.impl.std;
+
+import org.zamia.ZamiaProject;
+import org.zamia.plugin.tool.vhdl.rules.RuleE;
+import org.zamia.plugin.tool.vhdl.rules.RuleResult;
+import org.zamia.plugin.tool.vhdl.rules.impl.Rule;
+import org.zamia.util.Pair;
+
+public class RuleSTD_05900 extends Rule {
+
+	protected RuleSTD_05900() {
+		super(RuleE.STD_05900);
+	}
+
+	@Override
+	public Pair<Integer, RuleResult> Launch(ZamiaProject zPrj, String ruleId, ParameterSource parameterSource) {
+		
+		return null;
+	}
+
+}
